77f5c4581968fef2f4b653dbc24579fe59516675
Marcus Klingborg <marcus.klingborg@live.jp>
2023-01-20 09:09:15 +0100

    Started with queries

---
+++ db_queries/select_from_db/select_from_location.py
@@ -0,0 +1,38 @@
+#! python3
+
+# Import necessary libraries
+import psycopg2
+
+
+def select_from_location(conn, lat, lon):
+    query = f"SELECT station_id FROM weather.location WHERE latitude = '{lat}' AND longitude = '{lon}';"
+    value_id = ""
+
+    try:
+        # Create a cursor.
+        cur = conn.cursor()
+
+        # Executing a statement.
+        cur.execute(query)
+
+        # Check return from executed statement.
+        rows = cur.fetchall()
+
+        if rows == []:
+            print("It seems as if there is no matching category.")
+        else:
+            for row in rows:
+                print("Category found.")
+                print(row)
+                value_id = row
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+
+        cur.close()
+
+        if value_id == "":
+            value_id = None
+        return value_id
---
+++ db_queries/select_from_db/select_from_precipation_category.py
@@ -0,0 +1,38 @@
+#! python3
+
+# Import necessary libraries
+import psycopg2
+
+
+def select_from_precipation_category(conn, value):
+    query = f"SELECT category_id FROM weather.precipation_category WHERE meaning = '{value}';"
+    value_id = ""
+
+    try:
+        # Create a cursor.
+        cur = conn.cursor()
+
+        # Executing a statement.
+        cur.execute(query)
+
+        # Check return from executed statement.
+        rows = cur.fetchall()
+
+        if rows == []:
+            print("It seems as if there is no matching category.")
+        else:
+            for row in rows:
+                print("Category found.")
+                print(row)
+                value_id = row
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+
+        cur.close()
+
+        if value_id == "":
+            value_id = None
+        return value_id
